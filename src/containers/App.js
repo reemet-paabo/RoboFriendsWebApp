5643f1de613ee42fa0aaa14d0cc1caf2ce5e9bc2
reemet-paabo <reemet.coding@gmail.com>
2025-04-18 15:26:17 +0300

    using App() and hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import Title from "../components/Title";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
@@ -6,42 +6,42 @@ import Scroll from "../components/Scroll";
 import ErrorBoundry from "../components/ErrorBoundry";
 import "./app.css";
 
-class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			robots: [],
-			searchFields: "",
-		};
-	}
-	componentDidMount() {
+function App() {
+	const [robots, setRobots] = useState([]);
+	const [count, setCount] = useState(0);
+	const [searchFields, setSearchFields] = useState("");
+
+	useEffect(() => {
 		fetch("https://jsonplaceholder.typicode.com/users")
 			.then((response) => response.json())
-			.then((users) => this.setState({ robots: users }));
-	}
+			.then((users) => setRobots(users));
+	}, [count]);
 
-	onSearchChange = (event) => {
-		this.setState({ searchFields: event.target.value });
+	const onSearchChange = (event) => {
+		setSearchFields(event.target.value);
 	};
-
-	render() {
-		const { robots, searchFields } = this.state;
-		const filteredRobots = robots.filter((robot) => {
-			return robot.name.toLocaleLowerCase().includes(searchFields.toLocaleLowerCase());
-		});
-		return !robots.length ? (
-			<h3 className='f2'>Loading...</h3>
-		) : (
-			<div className='tc'>
-				<Title />
-				<SearchBox searchChange={this.onSearchChange} />
-				<Scroll>
-					<ErrorBoundry>
-						<CardList robots={filteredRobots}></CardList>
-					</ErrorBoundry>
-				</Scroll>
-			</div>
-		);
-	}
+	const filteredRobots = robots.filter((robot) => {
+		return robot.name.toLocaleLowerCase().includes(searchFields.toLocaleLowerCase());
+	});
+	return !robots.length ? (
+		<h3 className='f2'>Loading...</h3>
+	) : (
+		<div className='tc'>
+			<Title />
+			<button
+				onClick={() => {
+					setCount(count + 1);
+				}}
+			>
+				Click Me!
+			</button>
+			<SearchBox searchChange={onSearchChange} />
+			<Scroll>
+				<ErrorBoundry>
+					<CardList robots={filteredRobots}></CardList>
+				</ErrorBoundry>
+			</Scroll>
+		</div>
+	);
 }
 export default App;
