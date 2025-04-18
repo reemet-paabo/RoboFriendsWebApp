400ca732a4009c5fe49857e4160b0925b8298670
reemet-paabo <reemet.coding@gmail.com>
2025-04-18 14:06:50 +0300

    removed errorboundry testing statement

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -3,9 +3,6 @@ import Card from "./Card";
 
 const CardList = ({ robots }) => {
 	const list = robots.map(({ id, name, email }) => {
-		if (true) {
-			throw new Error("NOOO");
-		}
 		return <Card id={id} name={name} email={email} key={id} />;
 	});
 	return <div>{list}</div>;
